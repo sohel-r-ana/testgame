a56ebd04745bceb1a28cea56a6946699b7b1670c
xyz <96052143+sohel-r-ana@users.noreply.github.com>
2022-01-13 20:34:09 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+//there i programe the cube 
+function butntwo()
+{
+var num = Math.floor(Math.random())+ Math.floor(Math.random()*20+1);
+    // var numm = Math.floor(Math)
+    // var num = Math.floor(Math.random())+ Math.floor(Math.random()*6+1);
+
+document.getElementById("cube").innerHTML= num;
+
+    num == num;
+        var caaa = (42* num ) + "px";
+    document.getElementById("caa","cab","cac").style.top= "-"+ caaa;
+
+
+
+   
+}
+function butnone()
+{
+    var rand = Math.floor(Math.random()*6+1); 
+       var numb = rand - rand*2;
+
+document.getElementById("cube").innerHTML= numb;
+
+
+
+}
+
+
+
